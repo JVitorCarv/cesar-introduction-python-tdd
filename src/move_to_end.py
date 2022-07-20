8e3dab74a8d15c3e3c8b811eb46d900f52e045c2
Vitor <vitor.velozo2003@gmail.com>
2022-07-20 20:26:12 -0300

    Refactor parenthesis

--- src/move_to_end.py
+++ src/move_to_end.py
@@ -1,11 +1,12 @@
 class NotListError(Exception):
     pass
 
+
 def move_to_end(informed_list):
     if type(informed_list) is not list:
         raise NotListError(f'{informed_list} is not a list ')
 
-    if (len(informed_list) == 0):
+    if len(informed_list) == 0:
         return informed_list
 
     temp = informed_list[0]
